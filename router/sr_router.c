9f7c884f97290f4d09d77125c1161fd4eff3e7d8
moraneth <ethanmorann@gmail.com>
2022-10-15 18:58:35 -0400

    fix for s_addr attribute of in_aadr

--- router/sr_router.c
+++ router/sr_router.c
@@ -42,9 +42,8 @@ void sr_init(struct sr_instance* sr)
     while (next_node != NULL){
       strcpy(interface, next_node->interface);
       sr_add_interface(sr, interface);
-      //TODO: figure out how to convert from in_addr to unsigned int
       dest = next_node->dest;
-      sr_set_ether_ip(sr, dest.S_addr);
+      sr_set_ether_ip(sr, dest.s_addr);
       /*TODO: Need to call sr_arpcache_insert to find the mac address of each interface, as well as plug in destination*/
       next_node = next_node->next;
     }
