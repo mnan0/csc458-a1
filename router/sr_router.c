eb07a21b9f0864dab096b784f833448bdbc1f519
moraneth <ethanmorann@gmail.com>
2022-10-16 01:13:14 -0400

    free req packets

--- router/sr_router.c
+++ router/sr_router.c
@@ -39,7 +39,8 @@ void sr_init(struct sr_instance* sr)
     uint8_t* empty_packet=NULL;
     while (next_node != NULL){
       struct sr_arpreq* req = sr_arpcache_queuereq(&(sr->cache), next_node->dest.s_addr, empty_packet, 0, next_node->interface);
-      /*free(req);*/
+      free(req->packets);
+      /*TODO: Figure out what to do with the req*/
       next_node = next_node->next;
     }
 } /* -- sr_init -- */
