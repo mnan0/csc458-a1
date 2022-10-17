884588c6e6a4a43e665e94f463f6992c8162aae5
moraneth <ethanmorann@gmail.com>
2022-10-17 15:50:23 -0400

    free return req

--- router/sr_router.c
+++ router/sr_router.c
@@ -292,8 +292,9 @@ void sr_handlepacket(struct sr_instance* sr,
         struct sr_arpentry * matching_entry = sr_arpcache_lookup(&(sr->cache), echo_reply_dest);
         if (!matching_entry){
           /*No matching ARP entry, need to add a request and queue the packet*/
-          sr_arpcache_queuereq(&(sr->cache), echo_reply_dest, buf, sizeof(struct sr_ethernet_hdr) + sizeof(struct sr_ip_hdr) + sizeof(struct sr_icmp_hdr), interface);
+          struct sr_arpreq * return_req = sr_arpcache_queuereq(&(sr->cache), echo_reply_dest, buf, sizeof(struct sr_ethernet_hdr) + sizeof(struct sr_ip_hdr) + sizeof(struct sr_icmp_hdr), interface);
            /* Free memory */
+          free(return_req);
           free(buf);
           free(ethernet_hdr);
           free(ip_hdr);
