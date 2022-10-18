b9aaa674fe0bf7c718c47415140e229bb92ba421
Peter Sellars <p.sellars@mail.utoronto.ca>
2022-10-18 01:51:35 -0400

    commenting

--- router/sr_arpcache.c
+++ router/sr_arpcache.c
@@ -111,7 +111,7 @@ void handle_arprequest(struct sr_instance *sr, struct sr_arpreq *req) {
             arp_hdr->ar_hln = sizeof(dest_interface->addr);
             arp_hdr->ar_pln = sizeof(dest_interface->ip);
             arp_hdr->ar_op = htons(arp_op_request);
-            memcpy(arp_hdr->ar_sha,dest_interface->addr,sizeof(curr_if->addr));
+            memcpy(arp_hdr->ar_sha,dest_interface->addr,sizeof(dest_interface->addr));
             /*arp_hdr.ar_tha = NULL;*/
             memcpy(&(arp_hdr->ar_sip),&(dest_interface->ip),sizeof(uint32_t));
             memcpy(&(arp_hdr->ar_tip),&(req->ip),sizeof(uint32_t));
@@ -128,40 +128,40 @@ void handle_arprequest(struct sr_instance *sr, struct sr_arpreq *req) {
             /* Free memory */
             free(buf);
             curr_if = curr_if->next;
-            /*while(curr_if != NULL){
-                /* Set up ethernet header */
-                struct sr_ethernet_hdr* ethernet_hdr = malloc(sizeof(struct sr_ethernet_hdr));
-                uint8_t broadcast_mac[ETHER_ADDR_LEN] = {255,255,255,255,255,255};
-                memcpy(ethernet_hdr->ether_dhost, broadcast_mac, sizeof(broadcast_mac));
-                memcpy(ethernet_hdr->ether_shost,curr_if->addr, sizeof(curr_if->addr));
-                ethernet_hdr->ether_type= htons(ethertype_arp);
+            // /* while(curr_if != NULL){
+            //     /* Set up ethernet header */
+            //     struct sr_ethernet_hdr* ethernet_hdr = malloc(sizeof(struct sr_ethernet_hdr));
+            //     uint8_t broadcast_mac[ETHER_ADDR_LEN] = {255,255,255,255,255,255};
+            //     memcpy(ethernet_hdr->ether_dhost, broadcast_mac, sizeof(broadcast_mac));
+            //     memcpy(ethernet_hdr->ether_shost,curr_if->addr, sizeof(curr_if->addr));
+            //     ethernet_hdr->ether_type= htons(ethertype_arp);
 
                 
-                /* Set up ARP header */
-                struct sr_arp_hdr* arp_hdr = malloc(sizeof(struct sr_arp_hdr));
-                arp_hdr->ar_hrd = htons(arp_hrd_ethernet);
-                arp_hdr->ar_pro = htons(ethertype_ip);
-                arp_hdr->ar_hln = sizeof(curr_if->addr);
-                arp_hdr->ar_pln = sizeof(curr_if->ip);
-                arp_hdr->ar_op = htons(arp_op_request);
-                memcpy(arp_hdr->ar_sha,curr_if->addr,sizeof(curr_if->addr));
-                /*arp_hdr.ar_tha = NULL;*/
-                memcpy(&(arp_hdr->ar_sip),&(curr_if->ip),sizeof(uint32_t));
-                memcpy(&(arp_hdr->ar_tip),&(req->ip),sizeof(uint32_t));
+            //     /* Set up ARP header */
+            //     struct sr_arp_hdr* arp_hdr = malloc(sizeof(struct sr_arp_hdr));
+            //     arp_hdr->ar_hrd = htons(arp_hrd_ethernet);
+            //     arp_hdr->ar_pro = htons(ethertype_ip);
+            //     arp_hdr->ar_hln = sizeof(curr_if->addr);
+            //     arp_hdr->ar_pln = sizeof(curr_if->ip);
+            //     arp_hdr->ar_op = htons(arp_op_request);
+            //     memcpy(arp_hdr->ar_sha,curr_if->addr,sizeof(curr_if->addr));
+            //     /*arp_hdr.ar_tha = NULL;*/
+            //     memcpy(&(arp_hdr->ar_sip),&(curr_if->ip),sizeof(uint32_t));
+            //     memcpy(&(arp_hdr->ar_tip),&(req->ip),sizeof(uint32_t));
                 
-                uint8_t* buf = malloc(sizeof(struct sr_arp_hdr) + sizeof(struct sr_ethernet_hdr));
-                memcpy(buf, ethernet_hdr, sizeof(*ethernet_hdr));
-                memcpy(buf + sizeof(*ethernet_hdr), arp_hdr, sizeof(*arp_hdr));
-                free(ethernet_hdr);
-                free(arp_hdr);
+            //     uint8_t* buf = malloc(sizeof(struct sr_arp_hdr) + sizeof(struct sr_ethernet_hdr));
+            //     memcpy(buf, ethernet_hdr, sizeof(*ethernet_hdr));
+            //     memcpy(buf + sizeof(*ethernet_hdr), arp_hdr, sizeof(*arp_hdr));
+            //     free(ethernet_hdr);
+            //     free(arp_hdr);
 
-                /*print_hdrs(buf, sizeof(struct sr_arp_hdr) + sizeof(struct sr_ethernet_hdr));*/
-                sr_send_packet(sr, buf, sizeof(struct sr_arp_hdr) + sizeof(struct sr_ethernet_hdr),curr_if->name);
+            //     /*print_hdrs(buf, sizeof(struct sr_arp_hdr) + sizeof(struct sr_ethernet_hdr));*/
+            //     sr_send_packet(sr, buf, sizeof(struct sr_arp_hdr) + sizeof(struct sr_ethernet_hdr),curr_if->name);
 
-                /* Free memory */
-                free(buf);
-                curr_if = curr_if->next;
-            }*/
+            //     /* Free memory */
+            //     free(buf);
+            //     curr_if = curr_if->next;
+            // } */
             
 
             req->sent = now;
