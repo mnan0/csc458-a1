5d476855f876286c50f49934ac53150f494d734b
moraneth <ethanmorann@gmail.com>
2022-10-18 16:42:11 -0400

    used wrong ip header

--- router/sr_router.c
+++ router/sr_router.c
@@ -282,7 +282,7 @@ void sr_handlepacket(struct sr_instance* sr,
         icmp_hdr->unused = 0;
         icmp_hdr->next_mtu = 1500;
 
-        memcpy(icmp_hdr->data,  (uint8_t*) ip_hdr, sizeof(struct sr_ip_hdr));
+        memcpy(icmp_hdr->data,  (uint8_t*) curr_packet_ip_hdr, sizeof(struct sr_ip_hdr));
         memcpy(icmp_hdr->data + sizeof(struct sr_ip_hdr), (uint8_t*) (packet + sizeof(struct sr_ethernet_hdr) + sizeof(struct sr_ip_hdr)), 8);
         icmp_hdr->icmp_sum = cksum(icmp_hdr, ntohs(ip_hdr->ip_len) - (ip_hdr->ip_hl * 4)); 
         
