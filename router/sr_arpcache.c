dc3b53f08428075cd921f74c61e57e86f767b4b2
moraneth <ethanmorann@gmail.com>
2022-10-18 22:30:42 -0400

    size of ip_hl

--- router/sr_arpcache.c
+++ router/sr_arpcache.c
@@ -58,7 +58,7 @@ void handle_arprequest(struct sr_instance *sr, struct sr_arpreq *req) {
                 ip_hdr->ip_off = htons(IP_DF); /* if this causes problems, try IP_RF*/
                 ip_hdr->ip_ttl = INIT_TTL;
                 ip_hdr->ip_p = ip_protocol_icmp;
-                ip_hdr->ip_hl = 4;
+                ip_hdr->ip_hl = sizeof(struct sr_ip_hdr) / 4;
                 ip_hdr->ip_v = 4;   
                 memcpy(&(ip_hdr->ip_src), &(new_source->ip), sizeof(new_source->ip));
                 memcpy(&(ip_hdr->ip_dst), &(curr_packet_ip_hdr->ip_src), sizeof(curr_packet_ip_hdr->ip_src)); 
